716dabdc7a904da28e6c41a11403170101878095
danielemurgioni <dashomew@gmail.com>
2025-03-14 14:02:14 +0100

    [DONE] 02-strings; second exercise

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -10,6 +10,11 @@ console.log(name);
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+let number = 2001;
+let text = "nascità";
+let bool = true;
+console.log(number, text, bool);
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
